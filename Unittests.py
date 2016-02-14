ad34b67e602211a56d4e8e79bd8659dd207c4483
fkibler-tgm <flokib1997@gmail.com>
2016-02-14 20:41:15 +0100

    Revert "Unitest added to File Unittests.py"
    This reverts commit 78bd015469bcefa79961269f8e73c60fb2389163.

--- Unittests.py
+++ Unittests.py
@@ -4,26 +4,10 @@ import unittest
 
 
 class MyTestCase(unittest.TestCase):
-    """
     def test_something(self):
         self.assertEqual(True, False)
-    """
 
-    def test_UnterschiedlicheDialekte(selfself):
-        pass;
-
-    def test_Dateinichtvorhanden(self):
-        pass;
-
-    def test_UngueltigerDateiinhalt(self):
-        pass;
-
-    def test_KeinDateiinhalt(self):
-        pass;
-
-    def test_KorrekteAusfuehrung(self):
-        pass;
-
+    def
 
 if __name__ == '__main__':
     unittest.main()
