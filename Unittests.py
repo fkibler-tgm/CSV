33b947e608dea11dba190aaedb65ab51665ec1c7
fkibler-tgm <flokib1997@gmail.com>
2016-02-14 18:34:18 +0100

    Added Unit-Tests

---
+++ Unittests.py
@@ -0,0 +1,13 @@
+__author__ = 'Florian Kibler'
+
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+    def
+
+if __name__ == '__main__':
+    unittest.main()
